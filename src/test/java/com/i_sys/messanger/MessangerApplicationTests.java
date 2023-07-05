9900a21bf465920a9093b1d58154b1d4907b0320
stacenko63 <amr_stacenko777@mail.ru>
2023-07-05 14:40:44 +0400

    Initial commit

---
+++ src/test/java/com/i_sys/messanger/MessangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i_sys.messanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
