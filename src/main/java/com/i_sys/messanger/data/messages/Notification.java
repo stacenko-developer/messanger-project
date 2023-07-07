cba58529a556715dbd28738fbc404358ff733a2e
stacenko63 <amr_stacenko777@mail.ru>
2023-07-07 11:35:25 +0400

    fix: fix errors

--- src/main/java/com/i_sys/messanger/data/messages/Notification.java
+++ src/main/java/com/i_sys/messanger/data/messages/Notification.java
@@ -1,10 +1,10 @@
 package com.i_sys.messanger.data.messages;
 
 import com.i_sys.messanger.data.BaseEntity;
-import com.i_sys.messanger.data.users.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +12,17 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@Comment("Уведомления")
 @Table(schema = "notification", name = "tr_notification")
 public class Notification extends BaseEntity {
 
     @Column(name = "text", nullable = false)
+    @Comment("Текст уведомления")
     private String text;
 
-    @ManyToOne
-    @JoinColumn(name = "sender_id", nullable = false)
-    private User sender;
+    @Column(name = "sender", nullable = false)
+    @Comment("Отправитель уведомления")
+    private String sender;
 
     @OneToMany(mappedBy = "notification", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<NotificationView> notificationViews = new ArrayList<>();
--- src/main/java/com/i_sys/messanger/data/users/User.java
+++ src/main/java/com/i_sys/messanger/data/users/User.java
@@ -6,6 +6,7 @@ import com.i_sys.messanger.data.messages.NotificationView;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +15,13 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
+@Comment("Пользователи")
 @Table(schema = "notification", name = "tr_user")
 public class User extends BaseEntity {
 
     @Column(name = "external_id", nullable = false)
+    @Comment("Внешний идентификатор пользователя")
     private UUID externalId;
-
-    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Notification> notifications = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<NotificationView> notificationViews = new ArrayList<>();
