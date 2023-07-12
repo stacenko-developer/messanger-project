51c37c6a9c5829cd6baa6893fee2e72052277ee3
stacenko63 <amr_stacenko777@mail.ru>
2023-07-12 11:37:55 +0400

    fix: fix sql query for getAllUnReadNotifications

--- src/main/java/com/i_sys/messanger/core/notifications/repositories/NotificationRepository.java
+++ src/main/java/com/i_sys/messanger/core/notifications/repositories/NotificationRepository.java
@@ -13,11 +13,11 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     List<Notification> findNotificationsByNotificationViews_UserId(UUID userId, Pageable pageable);
 
     @Query(nativeQuery = true,
-            value = "SELECT DISTINCT n.id, n.sender, n.text FROM notification.tr_notification n " +
-                    "JOIN notification.tr_notification_view nv ON n.id = nv.notification_id " +
-                    "WHERE n.id NOT IN " +
-                    "(SELECT n.id FROM notification.tr_notification n " +
-                    "JOIN notification.tr_notification_view nv ON n.id = nv.notification_id " +
-                    "WHERE nv.user_id = :userId)")
+            value = "SELECT DISTINCT n_first.id, n_first.sender, n_first.text FROM notification.tr_notification n_first " +
+                    "JOIN notification.tr_notification_view nv ON n_first.id = nv.notification_id " +
+                    "WHERE NOT EXISTS " +
+                    "(SELECT n_second.id FROM notification.tr_notification n_second " +
+                    "JOIN notification.tr_notification_view nv ON n_second.id = nv.notification_id " +
+                    "WHERE nv.user_id = :userId AND n_second.id = n_first.id)")
     List<Notification> findAllUnReadNotifications(UUID userId, Pageable pageable);
 }
